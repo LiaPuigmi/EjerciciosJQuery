405f17f213d6744ccd1c9eef38d7c003a6c9a9e4
Lia <liapuigmi@gmail.com>
2023-10-10 16:34:39 +0200

    cartas arreglo

--- Ejercicio carta/mainCard.js
+++ Ejercicio carta/mainCard.js
@@ -9,7 +9,7 @@ let array=['./card-front.png','./card-front2.jpg','./card-front1.png','./card-fr
         start.onclick=function(){
             shuffleArray(array);
             for (let index = 0; index < array.length; index++) {
-                /* console.log(front+index); */
+                console.log(front+index); 
                 document.getElementById(front+index).src=array[index];
                 
             }
